d88f8ff4fa1e54ced99c959e9da20f31da1c2783
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-03-22 17:50:50 +0200

    + Not-found-page

---
+++ src/pages/Not-found-page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NotFoundPage() {
+  return (
+    <div className="error-404">
+      <h1 className="page-title">404 page not found</h1>
+      <p>We are sorry but the page you are looking for does not exist.</p>
+    </div>
+  );
+}
+
+export default NotFoundPage;
