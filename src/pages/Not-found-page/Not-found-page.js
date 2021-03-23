6e3f7ffd4096f7a8594453ab2de89f70aaab804b
Yevhenii Zhyrov <zh.zhyrov@gmail.com>
2021-03-23 22:00:10 +0200

    + Not-found-page styles

--- src/pages/Not-found-page/Not-found-page.js
+++ src/pages/Not-found-page/Not-found-page.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
 import { Link } from 'react-router-dom';
+
+import styles from './Not-found-page.module.scss';
 
 function NotFoundPage() {
   return (
-    <div className="error-404">
-      <h1 className="page-title">404 page not found</h1>
+    <div className={styles['not-found-page']}>
+      <div className="page-title">
+        <span>404</span> page not found
+      </div>
       <p>We are sorry but the page you are looking for does not exist.</p>
       <Link to="/">Go Home</Link>
     </div>
